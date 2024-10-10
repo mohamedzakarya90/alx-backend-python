89e4d2f18e74209af6a53c06ae2b1bf8413c6f1b
mohamed zakarya mohamed farag <mohamed.zakrya.farag@gmail.com>
2024-10-10 11:53:08 -0700

    0x00-python_variable_annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+basic annotations : add
+function to writting type-annotated function add
+takes two float a- b and returning their sum as float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Type-annotated Function
+    """
+    return a + b

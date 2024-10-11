1edee5e1353d4976fff4ce13dc2212df87939642
mohamed zakarya mohamed farag <mohamed.zakrya.farag@gmail.com>
2024-10-10 19:47:35 -0700

    2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+basic annotations : floor
+writting type-annotated function floor with float argument
+returning floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    type-annotated function
+    floor
+    """
+    return int(n)
